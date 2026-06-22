c864370939745a9e97a037048a19a7bfe88fa2b5
augusto-arth <arthur.agrod@gmail.com>
2026-06-21 22:32:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const dataAlvo = new Date("December 13, 2028 00:00:00").getTime();
+
+function atualizarContagem() {
+
+    const agora = new Date().getTime();
+
+    const diferenca = dataAlvo - agora;
+
+    if (diferenca <= 0) {
+
+        document.getElementById("dias").textContent = "0";
+        document.getElementById("horas").textContent = "0";
+        document.getElementById("minutos").textContent = "0";
+        document.getElementById("segundos").textContent = "0";
+
+        return;
+    }
+
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
+    const horas = Math.floor(
+        (diferenca % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutos = Math.floor(
+        (diferenca % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const segundos = Math.floor(
+        (diferenca % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("dias").textContent = dias;
+    document.getElementById("horas").textContent = horas.toString().padStart(2, '0');
+    document.getElementById("minutos").textContent = minutos.toString().padStart(2, '0');
+    document.getElementById("segundos").textContent = segundos.toString().padStart(2, '0');
+}
+
+atualizarContagem();
+setInterval(atualizarContagem, 1000);
